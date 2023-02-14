b83029c34a97bd48c7cd7f3687d65e9e09bfacf8
cole0 <cole0@DESKTOP-NCEQT1D>
2023-02-14 11:43:37 -0700

    Add project files.

---
+++ TrouserCoin/Block.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using Newtonsoft.Json;
+using EllipticCurve;
+
+namespace TrouserCoin
+{
+    class Block
+    {
+        public int Index { get; set; }
+        public string previousHash { get; set; }
+        public string timestamp { get; set; }
+        public string Hash { get; set; }
+        public int Nonce { get; set; }
+        public List<Transaction> Transactions { get; set; }
+
+        public Block(int Index, string timestamp, List<Transaction> transactions, string previousHash = "")
+        {
+            this.Index = Index;
+            this.timestamp = timestamp;
+            this.Transactions = transactions;
+            this.previousHash = previousHash;
+            this.Hash = CalculateHash();
+            this.Nonce = 0;
+        }
+
+        public string CalculateHash()
+        {
+            string blockData = this.Index + this.previousHash + this.timestamp + this.Transactions.ToString() + this.Nonce; // NOCNE?
+            byte[] blockBytes = Encoding.ASCII.GetBytes(blockData); // gets byte array representation of the string
+            byte[] hashByte = SHA256.Create().ComputeHash(blockBytes); //Computes the hash from the byte array
+
+            return BitConverter.ToString(hashByte).Replace("-", "");
+        }
+
+        public void Mine(int difficulty)
+        {
+            while (this.Hash.Substring(0, difficulty) != new string('0', difficulty))
+            {  
+                this.Nonce++;
+                this.Hash = this.CalculateHash();
+                // Console.WriteLine("Mining: " + this.Hash);
+            }
+
+            Console.WriteLine("The block has been mined " + this.Hash);
+        }
+
+    }
+}
---
+++ TrouserCoin/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using Newtonsoft.Json;
+using EllipticCurve;
+
+namespace TrouserCoin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            PrivateKey key1 = new PrivateKey();
+            PublicKey wallet1 = key1.publicKey();
+
+            PrivateKey key2 = new PrivateKey();
+            PublicKey wallet2 = key2.publicKey();
+
+
+            Blockchain trouserCoin = new Blockchain(2, 100);
+
+            Console.WriteLine("Starting Miner up");
+            trouserCoin.MinePendingTransactions(wallet1);
+
+            decimal bal1 = trouserCoin.GetBalanceOfWallet(wallet1);
+
+            Console.WriteLine("Balance of Wallet 1 is $" + bal1.ToString() + " TrouserCoins");
+
+            Transaction tx1 = new Transaction(wallet1, wallet2, 10);
+
+            tx1.SignTransaction(key1);
+
+            trouserCoin.addPendingTransaction(tx1);
+
+            Console.WriteLine("Starting Miner up");
+            trouserCoin.MinePendingTransactions(wallet2);
+
+            bal1 = trouserCoin.GetBalanceOfWallet(wallet1);
+            decimal bal2 = trouserCoin.GetBalanceOfWallet(wallet2);
+
+            Console.WriteLine("Balance of Wallet 1 is $" + bal1.ToString() + " TrouserCoins");
+            Console.WriteLine("Balance of Wallet 2 is $" + bal2.ToString() + " TrouserCoins");
+            string blockJSON = JsonConvert.SerializeObject(trouserCoin, Formatting.Indented);
+            Console.WriteLine(blockJSON);
+
+            
+
+            if (trouserCoin.IsChainValid())
+            {
+                Console.WriteLine("trouserCoin is Valid");
+            }
+            else
+            {
+                Console.WriteLine("trouserCoin is Invalid -- NOT BASED");
+            }
+              
+            Console.ReadLine(); // Stops terminal so I can read it
+        }
+    }
+}
